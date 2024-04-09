50ee55734b51bd5e86491a4f7ff7c4a14c5f138b
Aryan <thtiwari.aryan@gmail.com>
2024-04-09 18:48:17 +0530

    string

---
+++ String.js
@@ -0,0 +1,51 @@
+
+//eval
+
+const result = eval("5 + 4")
+
+console.log(result);
+
+
+let x=10;
+let y=20;
+
+console.log(x,y);
+
+eval(`x=${y}`) //x=20
+
+console.log(x,y);
+
+console.log(eval("'3'+2"));
+
+
+const str = "This is a new string";
+
+console.log(str.substring(1,6));
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.split());
+console.log(str.charAt(3));
+
+const res=(str.concat("  new String"))
+console.log(res);
+
+const num= parseInt("2345")
+console.log(num);
+
+const s = "   auhdiufds"
+
+console.log(s.trim());
+
+const d = "this is another one";
+
+console.log(d.replaceAll(" ",""));
+console.log(d.split(" ").join(""))
+
+
+
+
+
+
+
+
+
