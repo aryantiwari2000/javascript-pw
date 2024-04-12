7b582999cb28f6e52315a3e342cbdf2667188744
Aryan <thtiwari.aryan@gmail.com>
2024-04-13 00:30:33 +0530

    code added

--- async_code.js
+++ async_code.js
@@ -1,8 +1,28 @@
-for(let i=0; i<100000000; i++){}
-console.log("timer started");
+// for(let i=0; i<100000000; i++){}
+// console.log("timer started");
 
-setTimeout(()=>{
-console.log("work for set tmer");
-},10000)
+// setTimeout(()=>{
+// console.log("work for set tmer");
+// },10000)
 
-console.log("Timmer Triggered");
+// console.log("Timmer Triggered");
+
+for(let i = 0; i < 10000000000; i++) {
+    // some work
+}
+
+console.log("starting timer 1");
+setTimeout(() => {
+    console.log("timer 1 done");
+}, 20000);
+console.log("starting timer 2");
+setTimeout(() => {
+    console.log("timer 2 done")
+}, 0);
+for(let i = 0; i < 100000000000; i++) {
+    // some more work
+}
+console.log("starting timer 3");
+setTimeout(() => {
+    console.log("timer 3 done");
+}, 100);
